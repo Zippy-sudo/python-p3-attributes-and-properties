04b81b9d55a4839a9479ff664deb181571b70de6
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-01 23:26:24 +0300

    Cleaned Up

--- lib/person.py
+++ lib/person.py
@@ -18,23 +18,14 @@ APPROVED_JOBS = [
 class Person:
     
     def __init__(self, name="Joe", job="Admin"):
-        if isinstance(name, str) and 1 < len(name) < 25:
-            self._name = name.title()
-        
-        else:
-            print("Name must be string between 1 and 25 characters.")
-
-        if job in APPROVED_JOBS:
-            self._job = job
-        
-        else:
-            print("Job must be in list of approved jobs.")
+        self.name = name
+        self.job = job
 
     def get_name(self):
         return self._name
      
     def set_name(self, name):
-        if isinstance(name, str) and 1 < len(name) < 25:
+        if isinstance(name, str) and 1 < len(name) <= 25:
             self._name = name.title()
         
         else:
